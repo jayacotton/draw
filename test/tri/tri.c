7b3be14f4bd3edb91cd78fb8e81dfd3887eb600b
jaycotton <lbmgmusic@gmail.com>
2021-07-05 21:54:08 -0700

    updates to api

--- test/tri/tri.c
+++ test/tri/tri.c
@@ -1,28 +1,15 @@
 #include <math.h>
 #include <stdio.h>
 #include "gems.h"
-#include "/home/jay/projects/graphics/plot10/c/f2c.h"
-#include "/home/jay/projects/graphics/plot10/c/plot.h"
+#include "../../f2c.h"
+#include "../../plot.h"
 
 void main()
 {
-	initt_ (30);
-	movabs_(100,100);
-	drwabs_(300,100);
-	drwabs_(200,187);
-	drwabs_(100,100);
-	finitt_(0  ,768);
+	initt(30);
+	movabs(100,100);
+	drwabs(300,100);
+	drwabs(200,187);
+	drwabs(100,100);
+	finitt(0  ,768);
 }
-int count ;
-int adeout_(int len, int *buf)
-{
-        count = len;
-printf("count %d\n",count);
-        if(count == 0) 
-		return 0;
-        while(count--){
-                putchar(*buf++);
-        }
-        return 0;
-}
-
